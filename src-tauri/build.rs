ce487b0f163e441b0adce0accc1939af98db8e46
kerion546 <chankunsan@gmail.com>
2025-12-30 15:10:17 +0900

    fix(build): allow windows build by temporarily disabling macos capabilities

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,10 +1,3 @@
 fn main() {
-    let capabilities = if cfg!(target_os = "macos") {
-        vec!["default", "macos-specific-permissions"]
-    } else {
-        vec!["default"]
-    };
-
-    tauri_build::try_build(tauri_build::Attributes::new().capabilities(capabilities))
-        .expect("failed to run tauri-build");
+    tauri_build::build()
 }
